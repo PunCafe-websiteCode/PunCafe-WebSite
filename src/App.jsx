b5eda5ae3513764736c61a7cc22d06f4d8143587
Pone <insysiengmaiphouthasone@gmail.com>
2025-06-23 15:11:53 +0700

    web1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
 import ProfileWebS from "./jsx/ProfileWebS";
 import LogInToWebS from "./jsx/LogInToWebS";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home, Auth, Orders } from "./pages";
 
 const WebS = () => {
+  // const isOwnerPath = useLocation().pathname.includes("/owner");
   return (
-    <div>
-      <ProfileWebS/>
-      <LogInToWebS/>
-    </div>
+    <>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/Orders' element={<Orders />} />
+        </Routes>
+    </Router>
+    
+    </>
   )
 }
 export default WebS
